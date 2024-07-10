264e2949e86e807e9ec1eabdac5a6583d53c9c1d
NvDzung <dung3032003md@gmail.com>
2024-07-11 01:24:47 +0700

    Retrieve documents from two collection in MongoDB

--- src/main/java/dzung/trie/spell_checker/repository/WordRepository.java
+++ src/main/java/dzung/trie/spell_checker/repository/WordRepository.java
@@ -1,15 +1,12 @@
 package dzung.trie.spell_checker.repository;
 
-import dzung.trie.spell_checker.document.Document;
 import dzung.trie.spell_checker.document.WordDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface WordRepository extends MongoRepository<WordDocument, String> {
     @Query("{'key' : ?0}")
-    List<Document> findByKey(String key);
+    WordDocument findByKey(String key);
 }
--- src/main/java/dzung/trie/spell_checker/service/SpellCheckerService.java
+++ src/main/java/dzung/trie/spell_checker/service/SpellCheckerService.java
@@ -12,8 +12,11 @@ import dzung.trie.spell_checker.trie.TrieWordSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.print.Doc;
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,6 +32,57 @@ public class SpellCheckerService {
     @Autowired
     private TrieWordSearch trieWord;
 
+//    public List<String> suggest(String word) {
+//        List<String> suggests = new ArrayList<>();
+//        suggests.addAll(trieWord.getSuggest(word));
+//        suggests.addAll(trieQuote.getSuggest(word));
+//
+//        return suggests;
+//    }
+
+    public WordDocument wordSuggest(String word) {
+        WordDocument ans = null;
+        String[] keys = word.split("\\s+");
+        String key = keys[keys.length - 1];
+
+        for (int i = 0; i < key.length(); i++) {
+            WordDocument doc = wordRepo.findByKey(key.substring(0, i + 1));
+            if (doc != null)
+                ans = doc;
+        }
+        return ans;
+    }
+
+    public List<QuoteDocument> quoteSuggest(String word) {
+        List<QuoteDocument> ans = null;
+        String[] keys = word.split("\\s+");
+
+        StringBuilder builder = new StringBuilder();
+        for (String x : keys) {
+            builder.append(x).append(" ");
+            String key = builder.toString().trim();
+
+            List<QuoteDocument> docs = quoteRepo.findByKey(key);
+            if (docs.size() > 0)
+                ans = docs;
+        }
+        return ans;
+    }
+
+    /**
+     * @param word
+     * @return list document cause we implements two trie (wordTrie and quoteTrie). if we store document in same collection that can be duplicate key.
+     * Two collection would be chosen, but in quote_collection still got be duplicate key.
+     * I didn't figure out yet.
+     */
+    public List<Document> suggest(String word) {
+        String keyWord = word.toLowerCase().trim();
+        List<Document> suggests = new ArrayList<>();
+        suggests.add(wordSuggest(keyWord));
+        suggests.addAll(quoteSuggest(keyWord));
+
+        return suggests;
+    }
 
     public void saveWordSuggestions(String key, TrieNode curr) {
         if (curr.value.getSuggestions().size() > 0) {
@@ -65,6 +119,7 @@ public class SpellCheckerService {
     }
 
     public boolean saveDb() {
-        return saveSuggestions(trieWord, trieQuote);
+//        return saveSuggestions(trieWord, trieQuote);
+        return true;
     }
 }
