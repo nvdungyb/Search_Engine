c31a87923e78a76e8b1578cb2e148751bfd3eeb2
NvDzung <dung3032003md@gmail.com>
2024-07-15 22:34:03 +0700

    Incrementing with the Redis Sorted Set And algorithm to maintain our k limit

--- src/main/java/com/dzung/search_engine/service/SpellCheckerService.java
+++ src/main/java/com/dzung/search_engine/service/SpellCheckerService.java
@@ -1,6 +1,5 @@
 package com.dzung.search_engine.service;
 
-import com.dzung.search_engine.document.Document;
 import com.dzung.search_engine.document.QuoteDocument;
 import com.dzung.search_engine.document.Suggestion;
 import com.dzung.search_engine.document.WordDocument;
@@ -10,8 +9,10 @@ import com.dzung.search_engine.trie.TrieNode;
 import com.dzung.search_engine.trie.TrieQuoteSearch;
 import com.dzung.search_engine.trie.TrieWordSearch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.resps.Tuple;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,8 @@ public class SpellCheckerService {
     private WordRepository wordRepo;
     @Autowired
     private QuoteRepository quoteRepo;
+    @Value("${max_completions}")
+    private int maxCompletions;
 
     public List<String> wordSuggest(Jedis conn, String word) {
         String[] words = word.split("\\s+");
@@ -32,10 +35,11 @@ public class SpellCheckerService {
 
         for (int i = keys.length() - 1; i >= 0; i--) {
             String key = keys.substring(0, i + 1);
-            List<String> ans = conn.zrange(key, 0, -1);
+            List<String> ans = conn.zrevrange(key, 0, -1);
             if (ans.size() > 0)
-                return ans.parallelStream()
+                return ans.stream()                                                   // Keep the sequence in the redis.
                         .filter(str -> str.split("\\s+").length == 1)           // Just filter word.
+                        .limit(3)
                         .collect(Collectors.toList());
         }
         return new ArrayList<>();
@@ -53,9 +57,9 @@ public class SpellCheckerService {
         builder.delete(builder.length() - 1, len);
         for (int i = keys.length - 1; i >= 0; i--) {
             String key = builder.toString().trim();
-            List<String> ans = conn.zrange(key, 0, -1);
+            List<String> ans = conn.zrevrange(key, 0, -1);
             if (ans.size() > 0)
-                return ans.parallelStream()
+                return ans.stream()                                                  // Keep the sequence in the redis.
                         .filter(str -> str.split("\\s+").length > 1)            // Just filter quote.
                         .collect(Collectors.toList());
 
@@ -71,10 +75,77 @@ public class SpellCheckerService {
         String keyWord = word.toLowerCase().trim();
         List<String> suggests = new ArrayList<>();
 
-        suggests.addAll(wordSuggest(conn, word));
-        suggests.addAll(quoteSuggest(conn, word));
+        suggests.addAll(wordSuggest(conn, keyWord));
+        suggests.addAll(quoteSuggest(conn, keyWord));
 
         return suggests;
+    }
+
+    public void updateWordScore(Jedis conn, String completion) {
+        StringBuilder builder = new StringBuilder();
+        String word = completion.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            builder.append(word.charAt(i));
+            String key = builder.toString();
+
+            if (conn.exists(key) && !conn.type(key).equals("zset"))
+                conn.del(key);
+
+            if (conn.zcard(key) >= maxCompletions / 2) {
+                if (conn.zscore(key, completion) != null) {
+                    conn.zincrby(key, 1, completion);
+                } else {
+                    List<Tuple> tuples = conn.zrangeWithScores(key, 0, 0);
+                    for (Tuple tuple : tuples) {
+                        String minCompletion = tuple.getElement();
+                        int minScore = (int) tuple.getScore();
+                        conn.zrem(key, minCompletion);
+                        conn.zincrby(key, minScore + 1, completion);           // More information at notes: Guideline
+                    }
+                }
+            } else {
+                conn.zincrby(key, 1, completion);
+            }
+        }
+    }
+
+    public void updateQuoteScore(Jedis conn, String completion) {
+        String[] words = completion.toLowerCase().split(" ");
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            builder.append(words[i]).append(" ");
+            String key = builder.toString().trim();
+
+            if (conn.exists(key) && !conn.type(key).equals("zset"))
+                conn.del(key);
+
+            if (conn.zcard(key) >= maxCompletions / 2) {
+                if (conn.zscore(key, completion) != null) {
+                    conn.zincrby(key, 1, completion);
+                } else {
+                    List<Tuple> tuples = conn.zrangeWithScores(key, 0, 0);
+                    for (Tuple tuple : tuples) {
+                        String minCompletion = tuple.getElement();
+                        int minScore = (int) tuple.getScore();
+                        conn.zrem(key, minCompletion);
+                        conn.zadd(key, minScore + 1, completion);
+                    }
+                }
+            } else {
+                conn.zadd(key, 1, completion);
+            }
+        }
+    }
+
+    public void updateScore(Jedis conn, String message) {
+        String completion = message.trim().replace("\\s+", " ");
+
+        String[] words = completion.split(" ");
+        if (words.length == 1)
+            updateWordScore(conn, words[0]);
+        else
+            updateQuoteScore(conn, completion);
     }
 
     public void saveWordSuggestions(String key, TrieNode curr) {
