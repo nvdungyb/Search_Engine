139159bc9a2e59f353af63ba24d954934615c653
NvDzung <dung3032003md@gmail.com>
2024-07-20 15:14:10 +0700

    Rename QuoteDocument's attribute

--- src/main/java/com/dzung/search_engine/document/QuoteDocument.java
+++ src/main/java/com/dzung/search_engine/document/QuoteDocument.java
@@ -1,5 +1,6 @@
 package com.dzung.search_engine.document;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +14,13 @@ import java.util.List;
 @Data
 public class QuoteDocument implements Document {
     @Id
+    @JsonIgnore
     private String id;
-    private String key;
+    private String prefix;
     private List<Suggestion> value;
 
-    public QuoteDocument(String key, List<Suggestion> value) {
-        this.key = key;
+    public QuoteDocument(String prefix, List<Suggestion> value) {
+        this.prefix = prefix;
         this.value = value;
     }
 }
