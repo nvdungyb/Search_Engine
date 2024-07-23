2e1a6d2ab22b062cde603fdf86a9f7b9b54d440e
NvDzung <dung3032003md@gmail.com>
2024-07-23 15:24:23 +0700

    Handle role exception, data seeder

--- src/main/java/com/dzung/search_engine/service/mongo/RoleDataSeeder.java
+++ src/main/java/com/dzung/search_engine/service/mongo/RoleDataSeeder.java
@@ -1,2 +1,34 @@
-package com.dzung.search_engine.service.mongo;public class RoleDataSeeder {
+package com.dzung.search_engine.service.mongo;
+
+import com.dzung.search_engine.entity.mongo.ERole;
+import com.dzung.search_engine.entity.mongo.Role;
+import com.dzung.search_engine.repository.mongo.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RoleDataSeeder {
+    @Autowired
+    private RoleRepository roleRepo;
+
+    @EventListener
+    @Transactional
+    public void LoadRoles(ContextRefreshedEvent event) {
+        List<ERole> roles = Arrays.stream(ERole.values()).collect(Collectors.toList());
+
+        for (ERole role : roles) {
+            if (roleRepo.findByName(role.name()).isEmpty())
+                roleRepo.save(new Role(role));
+        }
+    }
 }
+/**
+ * Initialing data to work with when we deploy a new application or set up a new database.
+ */
--- src/main/java/com/dzung/search_engine/service/mongo/RoleFactory.java
+++ src/main/java/com/dzung/search_engine/service/mongo/RoleFactory.java
@@ -1,2 +1,26 @@
-package com.dzung.search_engine.service.mongo;public class RoleFactory {
+package com.dzung.search_engine.service.mongo;
+
+import com.dzung.search_engine.entity.mongo.ERole;
+import com.dzung.search_engine.entity.mongo.Role;
+import com.dzung.search_engine.exception.RoleNotFoundException;
+import com.dzung.search_engine.repository.mongo.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleFactory {
+    @Autowired
+    RoleRepository roleRepo;
+
+    public Role getInstance(String role) throws RoleNotFoundException {
+        switch (role) {
+            case "admin" -> {
+                return roleRepo.findByName(ERole.ROLE_ADMIN.name()).get();
+            }
+            case "user" -> {
+                return roleRepo.findByName(ERole.ROLE_USER.name()).get();
+            }
+            default -> throw new RoleNotFoundException("No role found for " + role);
+        }
+    }
 }
