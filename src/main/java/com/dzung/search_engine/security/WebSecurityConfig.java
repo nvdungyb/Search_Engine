0e8ab90413ee3bcb18867442fcb2327933d820b2
NvDzung <dung3032003md@gmail.com>
2024-07-23 12:19:28 +0700

    WebSecurityConfig

--- src/main/java/com/dzung/search_engine/security/WebSecurityConfig.java
+++ src/main/java/com/dzung/search_engine/security/WebSecurityConfig.java
@@ -4,7 +4,9 @@ import com.dzung.search_engine.service.mongo.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -43,6 +45,11 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
@@ -69,7 +76,7 @@ public class WebSecurityConfig {
  * 2. Extracting Credential:
  * + If the request matches the expected URL and method. the filter extracts the username and password from the request parameters (default: username and password).
  * <p>
- * 3. Creating Authentication:
+ * 3. Creating Authentication (not yet authenticated):
  * + The extracted username and password are used to create an 'UsernamePasswordAuthenticationToken'. This token is not yet authenticated but carries the credentials.
  * <p>
  * 4. Delegating Authentication:
@@ -77,10 +84,16 @@ public class WebSecurityConfig {
  * + The 'AuthenticationManager' usually delegates to an 'AuthenticationProvider' (such as 'DaoAuthenticationProvider') to perform the authentication.
  * <p>
  * 5. AuthenticationProvider:
- * + The 'AuthenticationProvider' uses a 'UserDetailsService' to load user details (including password) from a db.
+ * + The 'AuthenticationProvider' uses a 'UserDetailsService' to load UserDetails (including password) from a db.
  * + It then compares the provided password with stored password.
  * <p>
  * 6. Successful Authentication:
  * + If authentication is successful, The 'AuthenticationProvider' return a fully authenticated 'Authentication' Object.
+ * Fully authenticated 'Authentication' object.
+ * + Principal: Represents the authenticated user. Typically, this is a UserDetails object.
+ * + Credentials: The authenticated credentials, such as a password. After successful authenticated, this is often set to null.
+ * + Authorities: The user's granted authorities, typically represented as a list of GrantedAuthority objects.
+ * + Details: additional details about the authentication request, such as the request IP address.
+ * + Authenticated: a boolean flag indicating whether the user has been authenticated.
  * + The filter then store this authentication object in the 'SecurityContext', which is associated with the current session.
  */
