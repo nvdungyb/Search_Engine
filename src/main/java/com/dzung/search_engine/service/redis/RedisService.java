1473ff3e2d7a5d85750dc05280a1edad1e7f4fb4
NvDzung <dung3032003md@gmail.com>
2024-07-26 17:13:40 +0700

    Increase user experience

--- src/main/java/com/dzung/search_engine/service/redis/RedisService.java
+++ src/main/java/com/dzung/search_engine/service/redis/RedisService.java
@@ -2,7 +2,6 @@ package com.dzung.search_engine.service.redis;
 
 import com.dzung.search_engine.entity.redis.QuoteRedis;
 import com.dzung.search_engine.entity.redis.WordRedis;
-import com.dzung.search_engine.models.QuoteDocument;
 import com.dzung.search_engine.models.Suggestion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,17 +9,12 @@ import org.springframework.data.redis.connection.DataType;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.ObjectError;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.resps.Tuple;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class RedisService {
@@ -106,17 +100,42 @@ public class RedisService {
 
     public void saveToRedis(QuoteRedis quoteHash) {
         String key = "quote:" + quoteHash.getKey();
+        int prefixLen = quoteHash.getKey().length();
         for (Suggestion suggestion : quoteHash.getQuoteDocument().getValue()) {
-            template.opsForZSet().add(key, suggestion.getCompletion(), suggestion.getScore());
+            String completion = suggestion.getCompletion();
+            double score = suggestion.getScore();
+            template.opsForZSet().add(key, completion, score);
+
+            while (prefixLen < completion.length()) {
+                String extraKey = "quote:" + completion.substring(0, prefixLen + 1).toLowerCase();
+                template.opsForZSet().add(extraKey, completion, score);
+                expireKey(extraKey);
+                prefixLen += 1;
+            }
         }
-        template.expire(key, Duration.ofHours(publicTimeToLive));
+        expireKey(key);
     }
 
     public void saveToRedis(WordRedis wordHash) {
         String key = "word:" + wordHash.getKey();
+        int prefixLen = wordHash.getKey().length();
         for (Suggestion suggestion : wordHash.getWordDocument().getValue()) {
             template.opsForZSet().add(key, suggestion.getCompletion(), suggestion.getScore());
+
+            String completion = suggestion.getCompletion();
+            while (prefixLen < completion.length()) {
+                String extraKey = "word:" + completion.substring(0, prefixLen + 1).toLowerCase();
+                template.opsForZSet().add(extraKey, completion, suggestion.getScore());
+                expireKey(extraKey);
+                prefixLen += 1;
+            }
         }
-        template.expire(key, Duration.ofHours(publicTimeToLive));
+        expireKey(key);
+    }
+
+    public void expireKey(String key) {
+        if (!template.expire(key, Duration.ofHours(publicTimeToLive))) {
+            System.out.println("Can not set time to live for : " + key);
+        }
     }
 }
